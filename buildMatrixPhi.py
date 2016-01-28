adcdc1dde56e0744815bff3ddf58b27e4ce80f25
Ivan Ivashkovskiy <ivan290895@yandex.ru>
2016-01-28 23:14:50 +0300

    minor changes for files to work

--- buildMatrixPhi.py
+++ buildMatrixPhi.py
@@ -4,20 +4,30 @@ import codecs
 import sklearn
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
+import pickle
+import numpy
+
+def save_obj(obj, name ):
+    with open('./'+ name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def load_obj(name ):
+    with open('./' + name + '.pkl', 'rb') as f:
+        return pickle.load(f)
 
 def main():
 	with codecs.open("wallsDB.txt","r",encoding='utf-8') as f:
 		walls = f.read().split("\t\n\t")
-		vectorizer = CountVectorizer()#max_df=0.95, min_df=2)
+		vectorizer = CountVectorizer(max_df=0.95, min_df=2)
 		F = vectorizer.fit_transform(walls)
 		vocab = vectorizer.vocabulary_
-		stopWords = vectorizer.stop_words_
-		lda = LatentDirichletAllocation(n_topics=4, max_iter=7,
+
+		lda = LatentDirichletAllocation(n_topics=1000, max_iter=10,
                                 learning_method='online', learning_offset=30.,
                                 random_state=777)
 		lda.fit(F)
-		Phi = lda.components_
-		print Phi
+		save_obj(lda, "Phi")
+		save_obj(vocab, "vocab")
 
 if __name__ == "__main__":
 	main()
--- product.py
+++ product.py
@@ -6,7 +6,6 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 import pickle
 import numpy
-from numpy.linalg import solve
 from sklearn.neighbors import NearestNeighbors
 
 def loadFriends(myId):
@@ -17,12 +16,14 @@ def loadFriends(myId):
 
 def loadWalls(people):
 	walls =[]
+	accessedFriends = []
 	print "Getting wall posts"
 	for i, pId in enumerate(people):
 		answer = json.loads(urllib2.urlopen("https://api.vk.com/method/wall.get?owner_id="+str(pId)+"&offset=0&count=100&owner="+str(pId)).read())
 		if answer.has_key("error"):
 			continue
 
+		accessedFriends.append(pId)
 		print i*100.0/len(people), "%"
 		walls.append([])
 		posts = answer["response"][1:]
@@ -33,7 +34,7 @@ def loadWalls(people):
 			elif (post.has_key("text")):
 				walls[-1].append(post["text"])
 
-	return walls
+	return walls, accessedFriends
 
 def save_obj(obj, name ):
     with open('./'+ name + '.pkl', 'wb') as f:
@@ -43,32 +44,41 @@ def load_obj(name ):
     with open('./' + name + '.pkl', 'rb') as f:
         return pickle.load(f)
 
-def printSuperFriends(friends):
+def printSuperFriends(friends, accessedFriends):
 	for friend in friends:
-		friendList = json.loads(urllib2.urlopen("https://api.vk.com/method/users.get?user_id="+str(friend)).read())["response"][0]
+		friendList = json.loads(urllib2.urlopen("https://api.vk.com/method/users.get?user_ids="+str(accessedFriends[friend])).read())["response"][0]
 		print friendList["first_name"], friendList["last_name"]
 
 
 def main():
 	myId = 69827345
 	friends = loadFriends(myId)#[69827345, 212417383, 86220855]
-	wallPosts = loadWalls(uniquePeople)
+	wallPosts, accessedFriends = loadWalls(friends)
 	walls = []
 	for wall in wallPosts:
 		walls.append(" ".join(wall))
 
 	vocab = load_obj("vocab")
-	Phi = load_obj("Phi")
+	lda = load_obj("Phi")
 
 	vectorizer = CountVectorizer(vocabulary=vocab)
-	matr = numpy.transpose(vectorizer.transform(walls))
-	features = numpy.transpose(solve(Phi, matr))
+
+	allFeatures = []
+	for i,wall in enumerate(walls):
+		userWords = vectorizer.transform([wall])
+		features = lda.transform(userWords)[0]
+		allFeatures.append(features)
+
+	featuresNumpy = numpy.zeros((len(allFeatures),allFeatures[0].shape[0]))
+	for i in xrange(featuresNumpy.shape[0]):
+		for j in xrange(featuresNumpy.shape[1]):
+			featuresNumpy[i,j] = allFeatures[i][j]
 
 	#find nearest to me
 	neigh = NearestNeighbors()
-	neigh.fit(features)
-	mySuperFriends = neigh.kneighbors(features[-1], 10, return_distance=False)
-	printSuperFriends(mySuperFriends)
+	neigh.fit(featuresNumpy)
+	mySuperFriends = neigh.kneighbors(featuresNumpy[-1].reshape(1,-1), 25, return_distance=False)[0]
+	printSuperFriends(mySuperFriends, accessedFriends)
 
 if __name__ == "__main__":
 	main()
