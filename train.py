56d76e94ed4426a4cc6e4d41b96453b6c0c80eee
Ivan Ivashkovskiy <ivan290895@yandex.ru>
2016-01-28 19:34:57 +0300

    wallpost getter

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import urllib2
 import json
+import codecs
 
 def loadFriends(myId):
 	print "Getting friend list..."
@@ -15,9 +16,34 @@ def loadFriends(myId):
 	print "Number of unique FOF: ", len(uniquePeople)
 	return uniquePeople
 
+def loadWalls(people):
+	walls =[]
+	print "Getting wall posts of person:"
+	for pId in people:
+		walls.append([])
+		answer = json.loads(urllib2.urlopen("https://api.vk.com/method/wall.get?owner_id="+str(pId)+"&offset=0&count=100&owner="+str(pId)).read())
+		if answer.has_key("error"):
+			continue
+
+		print pId
+		posts = answer["response"][1:]
+		for post in posts:
+			if (post["post_type"] == "copy"):
+				if (post.has_key("copy_text")):
+					walls[-1].append(post["copy_text"])
+			elif (post.has_key("text")):
+				walls[-1].append(post["text"])
+
+	return walls
+
 def main():
 	myId = 69827345
-	uniquePeople = loadFriends(myId)
+	uniquePeople = loadFriends(myId)#[69827345, 212417383, 86220855]
+	wallPosts = loadWalls(uniquePeople)
+	with codecs.open("mytxt.txt","w",encoding='utf-8') as f:
+		for wall in wallPosts:
+			for post in wall:
+				f.write(post+"\r\n")
 
 if __name__ == "__main__":
 	main()
